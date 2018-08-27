a039caa9e27f2bc109fc6a16632aec222e9cbd6c
honfong <15714095+jerrychong25@users.noreply.github.com>
2018-08-27 18:02:46 +0800

    Added OnClickListener function

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        // find the view that show numbers category
+        TextView numbers = (TextView)findViewById(R.id.numbers);
+        // Set a clickListener on that view
+
+        numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // creat new intent for open (( numbeersActivity
+                Intent numbersIntent = new Intent(MainActivity.this,NumbersActivity.class);
+                // Start to new Activity
+                startActivity(numbersIntent);
+            }
+        });
+
     }
 }
