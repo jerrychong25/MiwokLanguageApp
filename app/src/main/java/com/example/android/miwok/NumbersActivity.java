d2c90e8500a0909d00c6dd8176fdc4129a21d7e7
honfong <15714095+jerrychong25@users.noreply.github.com>
2018-09-04 13:34:28 +0800

    Modified words from Array to ArrayList

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,9 +3,12 @@ package com.example.android.miwok;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -13,5 +16,45 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        // Create an arrayList of words
+        ArrayList<String> words = new ArrayList<String>();
+
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        Log.v("NumbersActivity", "word index 0: " + words.get(0));
+        Log.v("NumbersActivity", "word index 1: " + words.get(1));
+        Log.v("NumbersActivity", "word index 2: " + words.get(2));
+        Log.v("NumbersActivity", "word index 3: " + words.get(3));
+        Log.v("NumbersActivity", "word index 4: " + words.get(4));
+        Log.v("NumbersActivity", "word index 5: " + words.get(5));
+        Log.v("NumbersActivity", "word index 6: " + words.get(6));
+        Log.v("NumbersActivity", "word index 7: " + words.get(7));
+        Log.v("NumbersActivity", "word index 8: " + words.get(8));
+        Log.v("NumbersActivity", "word index 9: " + words.get(9));
+
+
+//        // Create an array of words
+//        String[] words = new String[10];
+//
+//        words[0] = "one";
+//        words[1] = "two";
+//        words[2] = "three";
+//        words[3] = "four";
+//        words[4] = "five";
+//        words[5] = "six";
+//        words[6] = "seven";
+//        words[7] = "eight";
+//        words[8] = "nine";
+//        words[9] = "ten";
     }
 }
