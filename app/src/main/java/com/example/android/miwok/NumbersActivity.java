1ae78e689cad8a5a18f843f17556b03c8c0eed0a
honfong <15714095+jerrychong25@users.noreply.github.com>
2018-09-24 13:24:05 +0800

    Added TextViews to layout with While Loop

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -32,49 +32,28 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        Log.v("NumbersActivity", "word index 0: " + words.get(0));
-        Log.v("NumbersActivity", "word index 1: " + words.get(1));
-        Log.v("NumbersActivity", "word index 2: " + words.get(2));
-        Log.v("NumbersActivity", "word index 3: " + words.get(3));
-        Log.v("NumbersActivity", "word index 4: " + words.get(4));
-        Log.v("NumbersActivity", "word index 5: " + words.get(5));
-        Log.v("NumbersActivity", "word index 6: " + words.get(6));
-        Log.v("NumbersActivity", "word index 7: " + words.get(7));
-        Log.v("NumbersActivity", "word index 8: " + words.get(8));
-        Log.v("NumbersActivity", "word index 9: " + words.get(9));
+//        Log.v("NumbersActivity", "word index 0: " + words.get(0));
+//        Log.v("NumbersActivity", "word index 1: " + words.get(1));
+//        Log.v("NumbersActivity", "word index 2: " + words.get(2));
+//        Log.v("NumbersActivity", "word index 3: " + words.get(3));
+//        Log.v("NumbersActivity", "word index 4: " + words.get(4));
+//        Log.v("NumbersActivity", "word index 5: " + words.get(5));
+//        Log.v("NumbersActivity", "word index 6: " + words.get(6));
+//        Log.v("NumbersActivity", "word index 7: " + words.get(7));
+//        Log.v("NumbersActivity", "word index 8: " + words.get(8));
+//        Log.v("NumbersActivity", "word index 9: " + words.get(9));
 
         LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
 
         int index = 0;
 
-        TextView wordView = new TextView(this);
-        wordView.setText(words.get(index));
-        rootView.addView(wordView);
+        while(index < words.size())
+        {
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(index));
+            rootView.addView(wordView);
 
-        index += 1;
-
-        TextView wordView2 = new TextView(this);
-        wordView2.setText(words.get(index));
-        rootView.addView(wordView2);
-
-        index += 1;
-
-        TextView wordView3 = new TextView(this);
-        wordView3.setText(words.get(index));
-        rootView.addView(wordView3);
-
-//        // Create an array of words
-//        String[] words = new String[10];
-//
-//        words[0] = "one";
-//        words[1] = "two";
-//        words[2] = "three";
-//        words[3] = "four";
-//        words[4] = "five";
-//        words[5] = "six";
-//        words[6] = "seven";
-//        words[7] = "eight";
-//        words[8] = "nine";
-//        words[9] = "ten";
+            index += 1;
+        }
     }
 }
