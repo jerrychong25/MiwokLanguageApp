ca14ddb0fcb87023193d036a457722764c84c075
Jerry Chong <15714095+jerrychong25@users.noreply.github.com>
2018-10-29 22:53:47 +0800

    Added remaining audio files and made it playable for all words

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -8,17 +8,21 @@ public class Word {
 
     private int mImageResourceId = NO_IMAGE_PROVIDED;
 
+    private int mAudioResourceId;
+
     private static final int NO_IMAGE_PROVIDED = -1;
 
-    public Word(String defaultTranslation, String miwokTranslation) {
+    public Word(String defaultTranslation, String miwokTranslation, int audioResourceId) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+        mAudioResourceId = audioResourceId;
     }
 
-    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId, int audioResourceId) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
         mImageResourceId = imageResourceId;
+        mAudioResourceId = audioResourceId;
     }
 
     public String getDefaulTranslation() {
@@ -32,4 +36,6 @@ public class Word {
     public int getImageResourceId(){ return mImageResourceId; }
 
     public boolean hasImage() { return mImageResourceId != NO_IMAGE_PROVIDED; }
+
+    public int getmAudioResourceId() { return mAudioResourceId; }
 }
